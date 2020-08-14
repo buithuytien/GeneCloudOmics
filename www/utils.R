c6ce5f1bf044c667661dfaa9d1175757ea589f33
rahul799 <rahulagrawal799110@gmail.com>
2020-08-14 10:56:28 +0530

    utils modified

--- www/utils.R
+++ www/utils.R
@@ -245,208 +245,6 @@ deseqDisp <- function(DS, f.df, ...) {
 
 
 
-############ GO ANALYSIS ##################
-############################################
-goPrep <- function(fg, bg, keyType, orgDb) {
-  # bg = background genes/ reference genes/ gene universe
-  # fg = foreground genes = DE genes to enrichment test
-  # orgDb = org.EcK12.eg.db (eg)
-  # keyType = "ENTREZID", "SYMBOL", etc...
-  all_genes <- keys(orgDb, keytype = keyType)
-  # all_genes_entrez <- keys(orgDb, keytype = "ENTREZID")
-  if (!any(fg %in% all_genes)) {
-    showModal(modalDialog(
-      title = "Error", "Input DE genes and identifier not matched. Please re-select the identifier and try again!"
-    ))
-    return(NULL)
-  }
-
-  if (is.null(bg)) {
-    bg_mapped <- all_genes
-  } else {
-    bg_mapped <- bg[bg %in% all_genes]
-  }
-  fg_mapped <- fg[fg %in% bg_mapped]
-  fg_unmapped <- fg[!fg %in% bg_mapped]
-  return(list(fg_mapped, bg_mapped, fg_unmapped))
-}
-
-enrichrApply <- function(gene_list, dbs, min_no = 1) {
-  res <- enrichr(gene_list, dbs)[[1]]
-  res$Count <- sapply(res$Genes, length) # convert to Count
-  # res <- filter(res, Count >= min_no)
-  return(res) # have Count
-}
-
-enrichgoApply <- function(gene_list, keyType, orgDb, ont = "BP", simpl = F, pvalueCutoff = 0.01, qvalueCutoff = 0.01) {
-  ego <- tryCatch(
-    {
-      ego <- enrichGO(
-        gene = gene_list,
-        keyType = keyType, # SYMBOL
-        OrgDb = orgDb, # org.EcK12.eg.db,
-        ont = ont,
-        pAdjustMethod = "BH",
-        pvalueCutoff = pvalueCutoff,
-        qvalueCutoff = qvalueCutoff
-      )
-      return(ego)
-    },
-    error = function(e) {
-      message("error occurs in clusterProfiler::enrichgo")
-      message("msg from line 278 utils")
-      return(NULL)
-    },
-    finally = {
-      message("Trycatch for clusterProfiler::enrichgo, msg from line 294 utils")
-    }
-  )
-
-  if (is.null(ego)) {
-    return(NULL)
-  } else {
-    if (simpl == T) {
-      ego <- simplify(ego, cutoff = 0.8, by = "p.adjust", select_fun = min)
-    }
-    return(as.data.frame(ego)) # have Count
-  }
-}
-
-gostatsApply <- function(fg_mapped, bg_mapped, keyType, orgDb, ont = "BP", primary_id = "ENTREZID", pvalueCutoff = 0.01) {
-  # bg_mapped = background genes
-  # fg_mapped = foreground genes
-
-  if (keyType != primary_id) {
-    bg_mapped <- mapIds(orgDb, bg_mapped, primary_id, keyType)
-    bg_mapped <- bg_mapped[!is.na(bg_mapped)]
-    fg_mapped <- mapIds(orgDb, fg_mapped, primary_id, keyType)
-    fg_mapped <- fg_mapped[!is.na(fg_mapped)]
-  }
-
-  hgCutoff <- 0.01
-  df <- tryCatch(
-    {
-      params <- new("GOHyperGParams",
-        geneIds = fg_mapped,
-        universeGeneIds = bg_mapped,
-        annotation = orgDb,
-        ontology = ont,
-        pvalueCutoff = pvalueCutoff,
-        conditional = FALSE,
-        testDirection = "over"
-      )
-      hgOver <- hyperGTest(params)
-      summary(hgOver)
-    },
-    error = function(e) {
-      cat("Error: ", e$message, "\n")
-      return(NULL)
-    },
-    finally = {
-      message("gostatsApply error handler")
-    }
-  )
-  return(df) # have Count
-}
-
-
-#### GO VISUALIZATION ###
-
-goToList <- function(go) {
-  # convert GO enrichment result (df) to list, with name being GO description, elements being gene ID
-  llist <- list()
-  for (i in 1:nrow(go)) {
-    nname <- as.character(go$Description)[i]
-    gg <- strsplit(as.character(go$geneID[i]), split = "/")
-    llist[[nname]] <- unlist(gg)
-  }
-  return(llist)
-}
-
-
-graphGene <- function(geneSets, group.name) {
-  # geneSets = named list of genes with name = name of go term
-  showCategory <- 5
-  foldChange <- NULL
-  layout <- "kk"
-  colorEdge <- FALSE
-  circular <- FALSE
-  node_label <- TRUE
-
-  g <- list2graph(geneSets)
-  size <- sapply(geneSets, length)
-  V(g)$size <- 1
-  n <- length(geneSets)
-  V(g)$size[1:n] <- size
-
-  igraph::V(g)$color <- "#B3B3B3" # gene nodes color
-  igraph::V(g)$color[1:n] <- makeColorBrewer(n) # distinctColorPalette(n)  # "#E5C494"  # bio process color
-  if (n == 1) {
-    igraph::V(g)$color[1] <- "#E5C494"
-  }
-
-  hubs <- V(g)$name[1:n] # names(geneSets);
-  cols <- igraph::V(g)$color
-  names(cols) <- "genes"
-  names(cols)[1:n] <- hubs
-
-  V(g)$name[1:n] <- " " # remove labels in the GO processes to avoid confusion
-
-  edge_layer <- geom_edge_link(alpha = .8, colour = "darkgrey") # edge color
-  p <- ggraph(g, layout = layout, circular = circular) +
-    edge_layer +
-    geom_node_point(aes_(color = ~ I(color), size = ~size), show.legend = T) +
-    theme_void()
-  #  geom_node_text(aes_(label=~name), repel=TRUE) # add labels for node
-
-  # p <- p + scale_size(range=c(3, 10), breaks=unique(round(seq(min(size), max(size), length.out=4))))
-
-  # add labels for biological process hubs
-  p <- p + scale_size(
-    range = c(3, 10), breaks = V(g)$size[1:n], name = NULL,
-    labels = names(cols)[1:n],
-    guide = guide_legend(override.aes = list(color = cols[1:n]))
-  ) +
-    labs(subtitle = paste(length(unique(unlist(geneSets))), "genes"))
-
-  if (!missing(group.name)) p <- p + ggtitle(group.name)
-
-  return(list(p, g))
-}
-
-makeColorBrewer <- function(n, name = "Set3") {
-  require(RColorBrewer)
-  max_n <- brewer.pal.info[rownames(brewer.pal.info) == name, "maxcolors"]
-  if (n <= max_n) {
-    cols <- brewer.pal(n, name)
-  } else {
-    col1 <- brewer.pal(max_n, name)
-    cols <- rep(col1, floor(n / max_n))
-    cols <- c(cols, col1[1:(n %% max_n)])
-  }
-  return(cols)
-}
-
-list2graph <- function(inputList) {
-  x <- list2df(inputList)
-  g <- graph.data.frame(x, directed = FALSE)
-  return(g)
-}
-
-
-list2df <- function(inputList) {
-  ldf <- lapply(1:length(inputList), function(i) {
-    data.frame(
-      categoryID = rep(
-        names(inputList[i]),
-        length(inputList[[i]])
-      ),
-      Gene = inputList[[i]]
-    )
-  })
-
-  do.call("rbind", ldf)
-}
 
 havingIP <- function() {
   if (.Platform$OS.type == "windows") {
@@ -470,15 +268,6 @@ loadPkg <- function() {
     print("Package shinythemes installed")
     library(shinythemes)
   }
-
-  # if(length(find.package(package = 'shinyjs',quiet = T))>0){
-  #   library(shinyjs)
-  # }else{
-  #   print("Package shinyjs not installed")
-  #   install.packages("shinyjs")
-  #   print("Package shinyjs installed")
-  #   library(shinyjs)
-  # }
 
   if (length(find.package(package = "corrplot", quiet = T)) > 0) {
     library(corrplot)
@@ -516,16 +305,6 @@ loadPkg <- function() {
     library(statmod)
   }
 
-
-  if (length(find.package(package = "RColorBrewer", quiet = T)) > 0) {
-    library(RColorBrewer)
-  } else {
-    print("Package RColorBrewer not installed")
-    install.packages("RColorBrewer")
-    print("Package RColorBrewer installed")
-    library(RColorBrewer)
-  }
-
   if (length(find.package(package = "ggplot2", quiet = T)) > 0) {
     library(ggplot2)
   } else {
@@ -533,15 +312,6 @@ loadPkg <- function() {
     install.packages("ggplot2")
     print("Package ggplot2 installed")
     library(ggplot2)
-  }
-
-  if (length(find.package(package = "corrplot", quiet = T)) > 0) {
-    library(corrplot)
-  } else {
-    print("Package corrplot not installed")
-    install.packages("corrplot")
-    print("Package corrplot installed")
-    library(corrplot)
   }
 
   if (length(find.package(package = "entropy", quiet = T)) > 0) {
@@ -563,7 +333,6 @@ loadPkg <- function() {
   }
 
   if (length(find.package(package = "RUVSeq", quiet = T)) == 0) {
-    # source("https://bioconductor.org/BiocManager::install.R")
     BiocManager::install("RUVSeq", update = FALSE)
   }
   library(RUVSeq)
@@ -572,38 +341,32 @@ loadPkg <- function() {
     library(edgeR)
   } else {
     print("Package edgeR not installed")
-    # source("https://bioconductor.org/BiocManager::install.R")
     BiocManager::install("edgeR", update = FALSE)
     print("Package edgeR installed")
     library(edgeR)
   }
 
   if (length(find.package(package = "DESeq2", quiet = T)) == 0) {
-    # source("https://bioconductor.org/BiocManager::install.R")
     BiocManager::install("DESeq2", update = FALSE)
   }
   library(DESeq2)
 
   if (length(find.package(package = "NOISeq", quiet = T)) == 0) {
-    # source("https://bioconductor.org/BiocManager::install.R")
     BiocManager::install("NOISeq", update = FALSE)
   }
   library(NOISeq)
 
   if (length(find.package(package = "NBPSeq", quiet = T)) == 0) {
-    # source("https://bioconductor.org/BiocManager::install.R")
     BiocManager::install("NBPSeq", update = FALSE)
   }
   library(NBPSeq)
 
   if (length(find.package(package = "AnnotationForge", quiet = T)) == 0) {
-    # source("https://bioconductor.org/BiocManager::install.R")
     BiocManager::install("AnnotationForge", update = FALSE)
   }
   library(AnnotationForge)
 
   if (length(find.package(package = "GOstats", quiet = T)) == 0) {
-    # source("https://bioconductor.org/BiocManager::install.R")
     BiocManager::install("GOstats", update = FALSE)
   }
   library(GOstats)
@@ -630,7 +393,6 @@ loadPkg <- function() {
     library(ComplexHeatmap)
   } else {
     print("Package ComplexHeatmap not installed")
-    # source("https://bioconductor.org/BiocManager::install.R")
     BiocManager::install("ComplexHeatmap", update = FALSE)
     print("Package ComplexHeatmap installed")
     library(ComplexHeatmap)
@@ -667,207 +429,15 @@ loadPkg <- function() {
     library("Biobase")
   } else {
     print("Package Biobase not installed")
-    # source("https://bioconductor.org/BiocManager::install.R")
     BiocManager::install("Biobase", update = FALSE)
     print("Package Biobase installed")
     library(org.Hs.eg.db)
-  }
-
-  if (length(find.package(package = "org.Hs.eg.db", quiet = T)) > 0) {
-    library("org.Hs.eg.db")
-  } else {
-    print("Package org.Hs.eg.db not installed")
-    # source("https://bioconductor.org/BiocManager::install.R")
-    BiocManager::install("org.Hs.eg.db", update = FALSE)
-    print("Package org.Hs.eg.db installed")
-    library(org.Hs.eg.db)
-  }
-
-  if (length(find.package(package = "org.Sc.sgd.db", quiet = T)) > 0) {
-    library(org.Sc.sgd.db)
-  } else {
-    print("Package org.Sc.sgd.db not installed")
-    # source("https://bioconductor.org/BiocManager::install.R")
-    BiocManager::install("org.Sc.sgd.db", update = FALSE)
-    print("Package org.Sc.sgd.db installed")
-    library(org.Sc.sgd.db)
-  }
-
-  if (length(find.package(package = "org.EcK12.eg.db", quiet = T)) > 0) {
-    library(org.EcK12.eg.db)
-  } else {
-    print("Package org.EcK12.eg.db not installed")
-    # source("https://bioconductor.org/BiocManager::install.R")
-    BiocManager::install("org.EcK12.eg.db", update = FALSE)
-    print("Package org.EcK12.eg.db installed")
-    library(org.EcK12.eg.db)
-  }
-
-  if (length(find.package(package = "org.Mm.eg.db", quiet = T)) > 0) { # mouse mus musculus
-    library(org.Mm.eg.db)
-  } else {
-    print("Package org.Mm.eg.db not installed")
-    # source("https://bioconductor.org/BiocManager::install.R")
-    BiocManager::install("org.Mm.eg.db", update = FALSE)
-    print("Package org.Mm.eg.db installed")
-    library(org.Mm.eg.db)
-  }
-
-  if (length(find.package(package = "org.Rn.eg.db", quiet = T)) > 0) { # rat rattus norvegicus
-    library(org.Rn.eg.db)
-  } else {
-    print("Package org.Rn.eg.db not installed")
-    # source("https://bioconductor.org/BiocManager::install.R")
-    BiocManager::install("org.Rn.eg.db", update = FALSE)
-    print("Package org.Rn.eg.db installed")
-    library(org.Rn.eg.db)
-  }
-
-  if (length(find.package(package = "org.Gg.eg.db", quiet = T)) > 0) { # chicken gallus gallus
-    library(org.Gg.eg.db)
-  } else {
-    print("Package org.Gg.eg.db not installed")
-    # source("https://bioconductor.org/BiocManager::install.R")
-    BiocManager::install("org.Gg.eg.db", update = FALSE)
-    print("Package org.Gg.eg.db installed")
-    library(org.Gg.eg.db)
-  }
-
-  if (length(find.package(package = "org.Dr.eg.db", quiet = T)) > 0) { # zebra fish
-    library(org.Dr.eg.db)
-  } else {
-    print("Package org.Dr.eg.db not installed")
-    # source("https://bioconductor.org/BiocManager::install.R")
-    BiocManager::install("org.Dr.eg.db", update = FALSE)
-    print("Package org.Dr.eg.db installed")
-    library(org.Dr.eg.db)
-  }
-
-  if (length(find.package(package = "org.Dm.eg.db", quiet = T)) > 0) { # fruit fly
-    library(org.Dm.eg.db)
-  } else {
-    print("Package org.Dm.eg.db not installed")
-    # source("https://bioconductor.org/BiocManager::install.R")
-    BiocManager::install("org.Dm.eg.db", update = FALSE)
-    print("Package org.Dm.eg.db installed")
-    library(org.Dm.eg.db)
-  }
-
-  if (length(find.package(package = "org.Ce.eg.db", quiet = T)) > 0) { # worm
-    library(org.Ce.eg.db)
-  } else {
-    print("Package org.Ce.eg.db not installed")
-    # source("https://bioconductor.org/BiocManager::install.R")
-    BiocManager::install("org.Ce.eg.db", update = FALSE)
-    print("Package org.Ce.eg.db installed")
-    library(org.Ce.eg.db)
-  }
-
-  if (length(find.package(package = "org.Ag.eg.db", quiet = T)) > 0) { # Anopheles
-    library(org.Ag.eg.db)
-  } else {
-    print("Package org.Ag.eg.db not installed")
-    # source("https://bioconductor.org/BiocManager::install.R")
-    BiocManager::install("org.Ag.eg.db", update = FALSE)
-    print("Package org.Ag.eg.db installed")
-    library(org.Ag.eg.db)
-  }
-
-  if (length(find.package(package = "org.At.tair.db", quiet = T)) > 0) { # arabidopsis
-    library(org.At.tair.db)
-  } else {
-    print("Package org.At.tair.db not installed")
-    # source("https://bioconductor.org/BiocManager::install.R")
-    BiocManager::install("org.At.tair.db", update = FALSE)
-    print("Package org.At.tair.db installed")
-    library(org.At.tair.db)
-  }
-
-  if (length(find.package(package = "org.Cf.eg.db", quiet = T)) > 0) { # canine
-    library(org.Cf.eg.db)
-  } else {
-    print("Package org.Cf.eg.db not installed")
-    # source("https://bioconductor.org/BiocManager::install.R")
-    BiocManager::install("org.Cf.eg.db", update = FALSE)
-    print("Package org.Cf.eg.db installed")
-    library(org.Cf.eg.db)
-  }
-
-  if (length(find.package(package = "org.Bt.eg.db", quiet = T)) > 0) { # bovine
-    library(org.Bt.eg.db)
-  } else {
-    print("Package org.Bt.eg.db not installed")
-    # source("https://bioconductor.org/BiocManager::install.R")
-    BiocManager::install("org.Bt.eg.db", update = FALSE)
-    print("Package org.Bt.eg.db installed")
-    library(org.Bt.eg.db)
-  }
-
-  if (length(find.package(package = "org.EcSakai.eg.db", quiet = T)) > 0) { # ecoli sakai
-    library(org.EcSakai.eg.db)
-  } else {
-    print("Package org.EcSakai.eg.db not installed")
-    # source("https://bioconductor.org/BiocManager::install.R")
-    BiocManager::install("org.EcSakai.eg.db", update = FALSE)
-    print("Package org.EcSakai.eg.db installed")
-    library(org.EcSakai.eg.db)
-  }
-
-  if (length(find.package(package = "org.Mmu.eg.db", quiet = T)) > 0) { # rhesus
-    library(org.Mmu.eg.db)
-  } else {
-    print("Package org.Mmu.eg.db not installed")
-    # source("https://bioconductor.org/BiocManager::install.R")
-    BiocManager::install("org.Mmu.eg.db", update = FALSE)
-    print("Package org.Mmu.eg.db installed")
-    library(org.Mmu.eg.db)
-  }
-
-  if (length(find.package(package = "org.Pf.plasmo.db", quiet = T)) > 0) { # Malaria
-    library(org.Pf.plasmo.db)
-  } else {
-    print("Package org.Pf.plasmo.db not installed")
-    # source("https://bioconductor.org/BiocManager::install.R")
-    BiocManager::install("org.Pf.plasmo.db", update = FALSE)
-    print("Package org.Pf.plasmo.db installed")
-    library(org.Pf.plasmo.db)
-  }
-
-  if (length(find.package(package = "org.Pt.eg.db", quiet = T)) > 0) { # chimp
-    library(org.Pt.eg.db)
-  } else {
-    print("Package org.Pt.eg.db not installed")
-    # source("https://bioconductor.org/BiocManager::install.R")
-    BiocManager::install("org.Pt.eg.db", update = FALSE)
-    print("Package org.Pt.eg.db installed")
-    library(org.Pt.eg.db)
-  }
-
-  if (length(find.package(package = "org.Ss.eg.db", quiet = T)) > 0) { # pig
-    library(org.Ss.eg.db)
-  } else {
-    print("Package org.Ss.eg.db not installed")
-    # source("https://bioconductor.org/BiocManager::install.R")
-    BiocManager::install("org.Ss.eg.db", update = FALSE)
-    print("Package org.Ss.eg.db installed")
-    library(org.Ss.eg.db)
-  }
-
-  if (length(find.package(package = "org.Xl.eg.db", quiet = T)) > 0) { # frog
-    library(org.Xl.eg.db)
-  } else {
-    print("Package org.Xl.eg.db not installed")
-    # source("https://bioconductor.org/BiocManager::install.R")
-    BiocManager::install("org.Xl.eg.db", update = FALSE)
-    print("Package org.Xl.eg.db installed")
-    library(org.Xl.eg.db)
   }
 
   if (length(find.package(package = "clusterProfiler", quiet = T)) > 0) {
     library(clusterProfiler)
   } else {
     print("Package clusterProfiler not installed")
-    # source("https://bioconductor.org/BiocManager::install.R")
     BiocManager::install("clusterProfiler", update = FALSE)
     print("Package clusterProfiler installed")
     library(clusterProfiler)
@@ -877,7 +447,6 @@ loadPkg <- function() {
     library(DOSE)
   } else {
     print("Package DOSE not installed")
-    # source("https://bioconductor.org/BiocManager::install.R")
     BiocManager::install("DOSE", update = FALSE)
     print("Package DOSE installed")
     library(DOSE)
@@ -887,7 +456,6 @@ loadPkg <- function() {
     library(AnnotationDbi)
   } else {
     print("Package AnnotationDbi not installed")
-    # source("https://bioconductor.org/BiocManager::install.R")
     BiocManager::install("AnnotationDbi", update = FALSE)
     print("Package clusterProfiler installed")
     library(AnnotationDbi)
@@ -938,3 +506,6 @@ loadPkg <- function() {
     library(matrixStats)
   }
 }
+
+#################### INSTALLATION SCRIPTS ###############
+#sudo su - -c "R -e \"BiocManager::install("c('RUVSeq','edgeR','DESeq2','NOISeq','NBPSeq','AnnotationForge','GOstats','ComplexHeatmap','Biobase','clusterProfiler','DOSE','AnnotationDbi'), update = FALSE)\""
