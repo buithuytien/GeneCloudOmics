f7ef60e172fc3fa4c7504b75668d20009d56b712
Bui Thuy Tien <BUITT@sifbi.a-star.edu.sg>
2020-09-21 17:30:31 +0800

    last commit from TT

--- ABioTrans.R
+++ ABioTrans.R
@@ -77,12 +77,12 @@ if (length(find.package(package = "reticulate", quiet = T)) > 0) {
 
 ####################### Dependencies For RAFSIL ###################################
 
-if (length(find.package(package = "RAFSIL", quiet = T)) > 0) {
-  library(RAFSIL)
-} else {
-  install.packages("RAFSIL")
-  library(RAFSIL)
-}
+# if (length(find.package(package = "RAFSIL", quiet = T)) > 0) {
+#   library(RAFSIL)
+# } else {
+#   install.packages("RAFSIL")
+#   library(RAFSIL)
+# }
 
 if (length(find.package(package = "gridGraphics", quiet = T)) > 0) {
   library(gridGraphics)
@@ -198,12 +198,12 @@ if (length(find.package(package = "remotes", quiet = T)) > 0) {
   library(remotes)
 }
 
-if (length(find.package(package = "maEndToEnd", quiet = T)) > 0) {
-  suppressPackageStartupMessages({library("maEndToEnd")})
-} else {
-  remotes::install_github("b-klaus/maEndToEnd", ref="master")
-  suppressPackageStartupMessages({library("maEndToEnd")})
-}
+# if (length(find.package(package = "maEndToEnd", quiet = T)) > 0) {
+#   suppressPackageStartupMessages({library("maEndToEnd")})
+# } else {
+#   remotes::install_github("b-klaus/maEndToEnd", ref="master")
+#   suppressPackageStartupMessages({library("maEndToEnd")})
+# }
 
 if (length(find.package(package = "oligoClasses", quiet = T)) > 0) {
     library(moments)
